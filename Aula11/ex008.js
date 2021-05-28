ace9afd3a41af33217512a627278d259b98f7e83
Diego Sousa Santos <diegosantosmeta@gmail.com>
2021-05-28 16:00:44 -0300

    aulas

---
+++ Aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}`)
+if(vel > 60){
+    console.log('Você ultrapassou a velocidade permitida. Multado!')
+}
+console.log('Diriga sempre cinto de segurança')
